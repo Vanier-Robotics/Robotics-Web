e977a66fca5658fc6629525188bcf1fed13b1bd1
CryoTechnic <cryogen65@gmail.com>
2019-12-26 21:27:36 -0500

    Delimited nav menu
    Created nav element and delimited it as per the layout sheet.

--- src/pages/home.en.js
+++ src/pages/home.en.js
@@ -7,10 +7,14 @@ import Layout from '../components/layout'
 {/*import Footer from '../components/Footer'*/}
 
 const HomePage = () => (
-    <Layout>
+    <div class="nav">
+        {/*Textbox code goes here*/}
+        <Layout>
         <p>This is a test element to make sure everything works!</p>
+        <p>If you saw the element above, this component is reserved for the navigation menu</p>
         <Link to='/'>Go back to the homepage</Link>
     </Layout>
+    </div>
 )
 
 export default HomePage
