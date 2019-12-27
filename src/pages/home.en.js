64fbf5d679d9019bb109b19dd96b0b8ecea212d6
CryoTechnic <cryogen65@gmail.com>
2019-12-26 20:01:38 -0500

    Created Empty Page
    No content, just base. Add content later.

---
+++ src/pages/home.en.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+import Layout from '../components/layout'
+
+{/*import Main from '../components/Main'*/}
+{/*import Footer from '../components/Footer'*/}
+
+const HomePage = () => (
+    <Layout>
+
+    </Layout>
+)
+
+export default HomePage
