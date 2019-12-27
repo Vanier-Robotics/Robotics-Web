571b96eb98aa0e057d6f05a3150219a7735dbd1a
CryoTechnic <cryogen65@gmail.com>
2019-12-26 20:47:12 -0500

    Added test text

--- src/pages/home.en.js
+++ src/pages/home.en.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
+
 import Layout from '../components/layout'
 
 {/*import Main from '../components/Main'*/}
@@ -7,7 +8,8 @@ import Layout from '../components/layout'
 
 const HomePage = () => (
     <Layout>
-
+        <p>This is a test element to make sure everything works!</p>
+        <Link to='/'>Go back to the homepage</Link>
     </Layout>
 )
 
