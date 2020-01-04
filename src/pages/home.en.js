2d346fee2da2987c3a822c8692c71b1d0e713a1e
shutterstacks <shutterstacks@gmail.com>
2020-01-04 12:58:29 -0500

    navigation is functionning

--- src/pages/home.en.js
+++ src/pages/home.en.js
@@ -1,23 +1,28 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import { Link } from 'gatsby'
+//import { Link } from 'gatsby'
 import pic01 from '../images/pic01.jpg'
 import Layout from '../components/layout'
-import Button from 'gatsby'
+//import Button from 'gatsby'
 
 {/*import Main from '../components/Main'*/}
 {/*import Footer from '../components/Footer'*/}
 
-const HomePage = () => (
-    <div class="nav">
-        <Layout>
-        <p>This is a test element to make sure everything works!</p>
-        <p>If you saw the element above, this component is reserved for the navigation menu</p>
-        <Link to='/'>Go back to the homepage</Link>
-    </Layout>
-    </div>
-)
 
+
+const HomePage = () =>  (
+<div>
+  <div id="nav">    
+       <script type = "text/javascript" src = "lamppost.js" ></script>
+  </div>
+  
+<div id="info" >
+
+
+</div>
+  </div>
+)
+  
 const Header = (props) => (
   <nav>
           <ul>
