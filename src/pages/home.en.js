5129de7bdf06e35e94205b0e2e2160672a9d0acb
shutterstacks <shutterstacks@gmail.com>
2020-01-04 15:47:33 -0500

    deleted useless stuff in en/home

--- src/pages/home.en.js
+++ src/pages/home.en.js
@@ -1,14 +1,5 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-//import { Link } from 'gatsby'
-import pic01 from '../images/pic01.jpg'
-//import Layout from '../components/layout'
-//import Button from 'gatsby'
-
-{/*import Main from '../components/Main'*/}
-{/*import Footer from '../components/Footer'*/}
-
-
 
 const HomePage = () => (
 <div id = "layout">
@@ -23,77 +14,6 @@ src="/en/vanierStreet" ></iframe>
   </div>
 )
   
-const Header = (props) => (
-  <nav>
-          <ul>
-              <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-          </ul>
-          </nav>
-)
-
-class Main extends React.Component {
-    render() {
-        let close = (
-            <div
-                className="close"
-                onClick={() => {
-                    this.props.onCloseArticle()
-                }}
-            ></div>
-        )
-    
-        return (
-            <div
-              ref={this.props.setWrapperRef}
-              id="main"
-              style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}
-            >
-              <article
-                id="intro"
-                className={`${this.props.article === 'intro' ? 'active' : ''} ${
-                  this.props.articleTimeout ? 'timeout' : ''
-                }`}
-                style={{ display: 'none' }}
-              >
-                <h2 className="major">Intro</h2>
-                <span className="image main">
-                  <img src={pic01} alt="" />
-                </span>
-                <p>
-                  Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
-                  aliquam facilisis ante interdum congue. Integer mollis, nisl amet
-                  convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
-                  magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
-                  By the way, check out my <a href="#work">awesome work</a>.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                  dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
-                  torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
-                  libero eu nibh porttitor fermentum. Nullam venenatis erat id
-                  vehicula viverra. Nunc ultrices eros ut ultricies condimentum.
-                  Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae
-                  dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in
-                  lectus. Pellentesque habitant morbi tristique senectus et netus et
-                  malesuada fames ac turpis egestas. In non lorem sit amet elit
-                  placerat maximus. Pellentesque aliquam maximus risus, vel sed
-                  vehicula.
-                </p>
-                {close}
-              </article>
-              
-            </div>
-        )
-    }
-}
-Main.propTypes = {
-  route: PropTypes.object,
-  article: PropTypes.string,
-  articleTimeout: PropTypes.bool,
-  onCloseArticle: PropTypes.func,
-  timeout: PropTypes.bool,
-  setWrapperRef: PropTypes.func.isRequired,
-}
 
 HomePage.propTypes = {
     onOpenArticle: PropTypes.func,
