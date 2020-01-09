227c42feaad5c1666784499be8040957f3cf34a3
shutterstacks <shutterstacks@gmail.com>
2020-01-09 17:56:08 -0500

    changed text size

--- src/pages/en/members/example.js
+++ src/pages/en/members/example.js
@@ -1,7 +1,7 @@
 import React from 'react'
 const Example = () => (
 <div>
-<h3>Person 1</h3>
+<h2>Person 1</h2>
 <img class="personimage" id="person-1" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA"></img>
 <div id="left"><p class="persontext">Sit nihil sapiente id. Deserunt unde laborum sed eveniet et non non. Est pariatur est vel dolorem. Aspernatur rerum autem corporis excepturi esse deleniti.</p></div>
 
