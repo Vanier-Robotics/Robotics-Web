f9090de2897b9621b85e51b54c0e11cf813aa49e
shutterstacks <shutterstacks@gmail.com>
2020-01-09 17:50:15 -0500

    slide-in profiles

--- src/pages/en/characters.js
+++ src/pages/en/characters.js
@@ -9,8 +9,9 @@ const SecondPage = () => (
         <img class="team-image" id="build-image" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA"></img>
       <p class="team-name">BUILD</p>
       <div class="team" id="build">
-      <p id="hovertext">HOVER OVER A PROFILE TO STOP IT</p>
         <div class="strip">
+        <div class="person"><Example></Example></div>
+        <div class="person"><Example></Example></div>
         <div class="person"><Example></Example></div>
         <div class="person"><Example></Example></div>
         <div class="person"><Example></Example></div>
@@ -21,7 +22,6 @@ const SecondPage = () => (
       <img class="team-image" id="kiosk-image" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA"></img>
       <p class="team-name">KIOSK</p>
       <div class="team" id="kiosk">
-      <p id="hovertext">HOVER OVER A PROFILE TO STOP IT</p>
         <div class="strip">
         <div class="person"><Example></Example></div>
         <div class="person"><Example></Example></div>
@@ -33,7 +33,6 @@ const SecondPage = () => (
       <img class="team-image" id="programming-image" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA"></img>
       <p class="team-name">PROGRAMMING</p>
       <div class="team" id="programming">
-        <p id="hovertext">HOVER OVER A PROFILE TO STOP IT</p>
         <div class="strip">
         <div class="person"><Example></Example></div>
         <div class="person"><Example></Example></div>
@@ -46,7 +45,6 @@ const SecondPage = () => (
       <img class="team-image" id="journalism-image" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA"></img>
       <p class="team-name">JOURNALISM</p>
      <div class="team" id="journalism">
-       <p id="hovertext">HOVER OVER A PROFILE TO STOP IT</p>
         <div class="strip">
         <div class="person"><Example></Example></div>
         <div class="person"><Example></Example></div>
@@ -59,7 +57,6 @@ const SecondPage = () => (
       <img class="team-image" id="video-image" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA"></img>
       <p class="team-name">VIDEO</p>
       <div class="team" id="video">
-        <p id="hovertext">HOVER OVER A PROFILE TO STOP IT</p>
         <div class="strip">
         <div class="person"><Example></Example></div>
         <div class="person"><Example></Example></div>
@@ -72,7 +69,6 @@ const SecondPage = () => (
       <img class="team-image" id="web-image" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA"></img>
       <p class="team-name">WEB</p>
       <div class="team" id="web">
-        <p id="hovertext">HOVER OVER A PROFILE TO STOP IT</p>
         <div class="strip">
         <div class="person"><Example></Example></div>
         <div class="person"><Example></Example></div>
