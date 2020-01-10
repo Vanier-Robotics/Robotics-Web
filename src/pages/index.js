0efa9daa03b81b3150d1031ec011075d322600c2
shutterstacks <shutterstacks@gmail.com>
2020-01-10 09:28:59 -0500

    a lot of things
    created a new gatsby project and copied the index.js and main.scss code there, the images are generated in public but arent linked proprely, instead of saying ./static/image.png it says /static/image.png. That was changed manually

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,32 @@
-import React from 'react'
-import wooden_c2 from '../images/wooden_c2.jpg'
+import React from "react"
+import Layout from "../components/layout"
 import F32 from '../images/F32.png'
 import E23 from '../images/E23.png'
 import crc1 from '../images/crc1.png'
+import wooden_c2 from '../images/wooden_c2.jpg'
 
-const FirstPage = () => (
-  <div id="main1">
+export default () => <Layout>
+      <div id="main1">
       <div id="bg"></div>
     <div id="cubes">
   <div id="strip1">
           <div id="fr" class="block">
              <a class="tooltip" href="/fr/home">
-                      <img src={F32} alt="French box selector" class="image"></img>
+                      <img id="french-image" src={F32} alt="French box selector" class="image"></img>
                       <p id="fr-text">Français</p>
                       </a>
                 
           </div>
-          <div id="en" class="block">
+         <div id="en" class="block">
                 <a class="tooltip" href="/en/home">
-                          <img src={E23} alt="English box selector" class="image" id="red"></img>
+                          <img id="english-image" src={E23} alt="English box selector" class="image" id="red"></img>
                               <p id="en-text">English</p></a>
      
               </div>
   </div>
   <div id="strip2">
   <div id="wood" class="block">
-              <img src={wooden_c2} alt="Welcome to Vanier Robotics 2020" class="image" id="image"></img> 
+               <img src={wooden_c2} alt="Welcome to Vanier Robotics 2020" class="image" id="image"></img> 
           </div></div></div>
    <footer>
    <a href="https://robo-crc.ca" target="_blank" data-toggle="tooltip" data-placement="top" title="Visit CRC's website for more information!">
@@ -34,6 +35,4 @@ const FirstPage = () => (
    </a>
 </footer>
 </div>
-)
-
-export default FirstPage
+    </Layout>
